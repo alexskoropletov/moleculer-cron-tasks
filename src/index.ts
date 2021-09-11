ab5ce7869846845a58735cf00b2565f9f918628d
Aleksandr Skoropletov <aleksandr.skoropletov@quantori.com>
2021-09-11 14:33:06 +0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import { ServiceSchema } from "moleculer";
+import cron, { ScheduledTask } from 'node-cron';
+import { Timezone } from 'tz-offset';
+
+export interface CronTask {
+  name: string;
+  cronTime: string;
+  timezone: Timezone | undefined;
+  task(): void;
+};
+
+const CronTasks: ServiceSchema = {
+  crons: [] as ScheduledTask[],
+  name: "cron-tasks",
+	created() {
+		this.crons = [];
+		if (this.schema.crons) {
+			this.crons = (this.schema.crons as CronTask[])
+        .map((job: CronTask) => cron.schedule(job.cronTime, job.task, { timezone: job.timezone }));
+		}
+		return this.Promise.resolve();
+	},
+	async started(): Promise<void> {
+		this.crons.map((job: ScheduledTask) => {
+			job.start();
+		});
+	},
+	async stopped(): Promise<void> {
+		this.crons.map((job: ScheduledTask) => {
+			job.stop();
+		});
+	}
+};
+
+export default CronTasks;
